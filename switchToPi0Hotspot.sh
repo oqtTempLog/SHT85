39c94a4c930b489407aee10defb569fe9df9d145
pi1 <225406810+oqtTempLog@users.noreply.github.com>
2025-08-13 16:07:55 +0200

    .

--- switchToPi0Hotspot.sh
+++ switchToPi0Hotspot.sh
@@ -7,7 +7,7 @@ else
 	nmcli device wifi list --rescan yes | grep -q "pi0hotspot"
 	if [ $? -eq 0 ]; then
 		echo "pi0 hotspot available. Connect to pi0..."
-		nmcli connection up "Wi-Fi to pi0 hotspot"
+		sudo nmcli connection up "Wi-Fi to pi0 hotspot"
 	else
 		echo "pi0 hotspot not available."
 	fi
